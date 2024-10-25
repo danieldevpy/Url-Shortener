224616b0053f637d02dd680c842bdf0fed1926da
Daniel <danielfernandes202@gmail.com>
2024-10-25 02:09:50 -0300

    make index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import "@/app/globals.css"
+import InfosCards from "@/app/components/celula/InfosCards"
+import UrlComponent from "@/app/components/celula/UrlComponent"
+
 export default function HomePage(){
-    return <h1> hello world </h1>
+    return (
+        <div className="body">
+            <header>
+                <h1>FastUrl</h1>
+            </header>
+            <main>
+                <UrlComponent/>
+            </main>
+            <footer>
+                <InfosCards/>                
+            </footer>
+        </div>
+    )
 }
